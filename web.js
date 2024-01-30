fe86b20c592c04820ab1e9f8cba0044a14e8eec1
esraa <esraasamirrr8888@gmail.com>
2024-01-30 23:57:56 +0200

    Add new folder

--- web.js
+++ web.js
@@ -1,1 +1,6 @@
-alert("hello");
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const content = document.querySelector('.content');
+    content.style.marginTop = header.offsetHeight + 'px';
+});
